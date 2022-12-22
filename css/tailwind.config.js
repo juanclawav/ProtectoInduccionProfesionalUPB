4b30858495bfc04f759651b86c3b916861e673d2
Juan Cla C <juanclaudiocarrascotapia@gmail.com>
2022-12-22 15:38:30 -0400

    Commit: Subir proyecto

---
+++ css/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:\Users\Juan Cla\Desktop\ChuPB\ProyectoInduccion\css\my-style.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
